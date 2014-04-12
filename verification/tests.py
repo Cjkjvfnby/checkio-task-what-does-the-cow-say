c41cb14c8b6208000c3d2fe487a992ce9440795f
Cjkjvfnby <Cjkjvfnby@gmail.com>
2014-04-12 23:16:38 +0400

    add test for multiple spaces

--- verification/tests.py
+++ verification/tests.py
@@ -34,10 +34,23 @@ expected_cowsay_many_lines = r'''
                 ||     ||
 '''
 
+expected_cowsay_with_multiple_spaces = '''
+ ______________________
+< With multiple spaces >
+ ----------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+'''
+
+
 cowsay_one_line =  'Checkio rulezz'
 cowsay_two_lines =  'A longtextwithonlyonespacetofittwolines.'
 cowsay_many_lines = ('Lorem ipsum dolor sit amet, consectetur adipisicing elit, '
                      'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+cowsay_with_multiple_spaces = 'With  multiple  spaces                                     '
 
 TESTS = {
     "Basics": [
@@ -52,6 +65,10 @@ TESTS = {
         {
             "input": cowsay_many_lines,
             "answer": cowsay_many_lines,
+        },
+        {
+            "input": cowsay_with_multiple_spaces,
+            "answer": expected_cowsay_with_multiple_spaces
         }
     ]
 }
