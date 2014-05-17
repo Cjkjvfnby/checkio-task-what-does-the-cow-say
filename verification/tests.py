4cbb0f0531cbe4728e1a45554bca4fb1be0fffd7
Cjkjvfnby <Cjkjvfnby@gmail.com>
2014-05-17 12:53:41 +0400

    fix wrong test

--- verification/tests.py
+++ verification/tests.py
@@ -67,24 +67,24 @@ expected_cowsay_dzen = r'''
 | orggre guna pbzcyrk. Pbzcyrk vf orggre  |
 | guna pbzcyvpngrq. Syng vf orggre guna   |
 | arfgrq. Fcnefr vf orggre guna qrafr.    |
-| Ernqnovyvgl pbhagf. Fcrpvny pnfrf nerag |
-| fcrpvny rabhtu gb oernx gur ehyrf.      |
-| Nygubhtu cenpgvpnyvgl orngf chevgl.     |
-| Reebef fubhyq arire cnff fvyragyl.      |
-| Hayrff rkcyvpvgyl fvyraprq. Va gur snpr |
-| bs nzovthvgl, ershfr gur grzcgngvba gb  |
-| thrff. Gurer fubhyq or bar-- naq        |
-| cersrenoyl bayl bar --boivbhf jnl gb qb |
-| vg. Nygubhtu gung jnl znl abg or        |
-| boivbhf ng svefg hayrff lbher Qhgpu.    |
-| Abj vf orggre guna arire. Nygubhtu      |
-| arire vf bsgra orggre guna *evtug* abj. |
-| Vs gur vzcyrzragngvba vf uneq gb        |
-| rkcynva, vgf n onq vqrn. Vs gur         |
-| vzcyrzragngvba vf rnfl gb rkcynva, vg   |
-| znl or n tbbq vqrn. Anzrfcnprf ner bar  |
-| ubaxvat terng vqrn -- yrgf qb zber bs   |
-\ gubfr!                                  /
+| Ernqnovyvgl pbhagf. Fcrpvny pnfrf       |
+| nera'g fcrpvny rabhtu gb oernx gur      |
+| ehyrf. Nygubhtu cenpgvpnyvgl orngf      |
+| chevgl. Reebef fubhyq arire cnff        |
+| fvyragyl. Hayrff rkcyvpvgyl fvyraprq.   |
+| Va gur snpr bs nzovthvgl, ershfr gur    |
+| grzcgngvba gb thrff. Gurer fubhyq or    |
+| bar-- naq cersrenoyl bayl bar --boivbhf |
+| jnl gb qb vg. Nygubhtu gung jnl znl abg |
+| or boivbhf ng svefg hayrff lbh'er       |
+| Qhgpu. Abj vf orggre guna arire.        |
+| Nygubhtu arire vf bsgra orggre guna     |
+| *evtug* abj. Vs gur vzcyrzragngvba vf   |
+| uneq gb rkcynva, vg'f n onq vqrn. Vs    |
+| gur vzcyrzragngvba vf rnfl gb rkcynva,  |
+| vg znl or n tbbq vqrn. Anzrfcnprf ner   |
+| bar ubaxvat terng vqrn -- yrg'f qb zber |
+\ bs gubfr!                               /
  -----------------------------------------
         \   ^__^
          \  (oo)\_______
